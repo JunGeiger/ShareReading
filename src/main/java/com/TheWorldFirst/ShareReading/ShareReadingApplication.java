29f5e461d3ad80c74394697d95c38e11fe4f10ba
JunGeiger <36657166+JunGeiger@users.noreply.github.com>
2019-12-31 16:04:56 +0800

    spring boot初始项目上传

---
+++ src/main/java/com/TheWorldFirst/ShareReading/ShareReadingApplication.java
@@ -0,0 +1,13 @@
+package com.TheWorldFirst.ShareReading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheWorldFirst/ShareReading/ShareReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheWorldFirst.ShareReading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
