a267db8ed4646917be83ffe59f0fb2dd76552a2e
JunGeiger <1922352338@qq.com>
2020-03-21 09:09:40 +0800

    爬虫

--- src/main/java/com/TheWorldFirst/ShareReading/service/BookService.java
+++ src/main/java/com/TheWorldFirst/ShareReading/service/BookService.java
@@ -4,5 +4,10 @@ import java.util.HashMap;
 
 public interface BookService {
 
+    /**
+     * 获取书籍信息，根据isbn
+     * @param isbn
+     * @return
+     */
     HashMap<String, Object> getBookInfoByIsbn(String isbn);
 }
